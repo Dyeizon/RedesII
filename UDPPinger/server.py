7d9b80ace0f5c484dab2230a188dad669826cf8f
dyeizon <dyeizonque@gmail.com>
2024-10-01 18:32:41 -0300

    start of UDP-Pinger

---
+++ UDPPinger/server.py
@@ -0,0 +1,21 @@
+from random import randint
+from socket import *
+
+HOST = '127.0.0.1'
+PORT = 12000
+
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind((HOST, PORT))
+print('Servidor rodando no endereço {0}:{1}'.format(HOST, PORT))
+
+while True:
+    rand = randint(0, 10)
+
+    message, address = serverSocket.recvfrom(1024)
+
+    message = message.upper()
+
+    if rand < 4:
+        continue
+
+    serverSocket.sendto(message, address)
